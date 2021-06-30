570f01769aad2d534643a725617124c55b4bff47
aless <aless@DESKTOP-MLN9QRR>
2021-06-30 19:51:36 -0300

    Adicionar arquivos de projeto.

---
+++ Converter/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picImagem = new System.Windows.Forms.PictureBox();
+            this.btnCarregarImagem = new System.Windows.Forms.Button();
+            this.btnConvertImage = new System.Windows.Forms.Button();
+            this.txtBoxView = new System.Windows.Forms.TextBox();
+            this.btnView = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picImagem
+            // 
+            this.picImagem.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picImagem.Location = new System.Drawing.Point(12, 62);
+            this.picImagem.Name = "picImagem";
+            this.picImagem.Size = new System.Drawing.Size(326, 267);
+            this.picImagem.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picImagem.TabIndex = 0;
+            this.picImagem.TabStop = false;
+            // 
+            // btnCarregarImagem
+            // 
+            this.btnCarregarImagem.Location = new System.Drawing.Point(12, 403);
+            this.btnCarregarImagem.Name = "btnCarregarImagem";
+            this.btnCarregarImagem.Size = new System.Drawing.Size(126, 23);
+            this.btnCarregarImagem.TabIndex = 1;
+            this.btnCarregarImagem.Text = "Open Image";
+            this.btnCarregarImagem.UseVisualStyleBackColor = true;
+            this.btnCarregarImagem.Click += new System.EventHandler(this.btnCarregarImagem_Click);
+            // 
+            // btnConvertImage
+            // 
+            this.btnConvertImage.Location = new System.Drawing.Point(199, 403);
+            this.btnConvertImage.Name = "btnConvertImage";
+            this.btnConvertImage.Size = new System.Drawing.Size(124, 23);
+            this.btnConvertImage.TabIndex = 2;
+            this.btnConvertImage.Text = "Convert Image";
+            this.btnConvertImage.UseVisualStyleBackColor = true;
+            this.btnConvertImage.Click += new System.EventHandler(this.btnConvertImage_Click);
+            // 
+            // txtBoxView
+            // 
+            this.txtBoxView.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtBoxView.Location = new System.Drawing.Point(384, 34);
+            this.txtBoxView.Multiline = true;
+            this.txtBoxView.Name = "txtBoxView";
+            this.txtBoxView.Size = new System.Drawing.Size(432, 716);
+            this.txtBoxView.TabIndex = 3;
+            // 
+            // btnView
+            // 
+            this.btnView.Location = new System.Drawing.Point(227, 350);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(75, 23);
+            this.btnView.TabIndex = 4;
+            this.btnView.Text = "View";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.btnView_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1051, 762);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.txtBoxView);
+            this.Controls.Add(this.btnConvertImage);
+            this.Controls.Add(this.btnCarregarImagem);
+            this.Controls.Add(this.picImagem);
+            this.Name = "Form1";
+            this.Text = "Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picImagem;
+        private System.Windows.Forms.Button btnCarregarImagem;
+        private System.Windows.Forms.Button btnConvertImage;
+        private System.Windows.Forms.TextBox txtBoxView;
+        private System.Windows.Forms.Button btnView;
+    }
+}
+
---
+++ Converter/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace Converter
+{
+    public partial class Form1 : Form
+    {
+        Image image;
+        Bitmap bitmap;
+
+        const int heightImg = 50;
+        const int widthImg = 50;
+
+        int[,] binaryImage = new int[widthImg, heightImg];
+
+        String txtBinaryImg;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnConvertImage.Enabled = false;
+        }
+
+        private void btnCarregarImagem_Click(object sender, EventArgs e)
+        {
+            txtBinaryImg = "";
+
+            ImportImage();
+            btnConvertImage.Enabled = true;
+        }
+
+        private void btnConvertImage_Click(object sender, EventArgs e)
+        {
+            SaveFileTxt();
+        }
+
+        private void btnView_Click(object sender, EventArgs e)
+        {
+            txtBoxView.Text = txtBinaryImg;
+        }
+
+        private void ImportImage()
+        {
+            OpenFileDialog ofd1 = new OpenFileDialog();
+            DialogResult re = ofd1.ShowDialog();
+
+            ofd1.Filter = "Image PNG | *.png";
+            ofd1.RestoreDirectory = true;
+
+            if (re == DialogResult.OK)
+            {
+                image = Image.FromFile(ofd1.FileName);
+                bitmap = (Bitmap)image;
+                picImagem.Image = bitmap;
+
+                GetPixelsImage();
+
+                FormatToText();
+            }
+        }
+
+        private void SaveFileTxt()
+        {
+            SaveFileDialog sfd1 = new SaveFileDialog();
+            StreamWriter writer = new StreamWriter("img.txt");
+
+            sfd1.Filter = "Text files | *.txt";
+            sfd1.RestoreDirectory = true;
+
+            if (sfd1.ShowDialog() == DialogResult.OK)
+            {
+                writer.Write(txtBinaryImg);
+                writer.Close();
+            }
+        }
+
+        private void FormatToText()
+        {
+            for (int colunm = 0; colunm < heightImg; colunm++)
+            {
+                txtBinaryImg = String.Format(txtBinaryImg + "\n");
+
+                for (int row = 0; row < widthImg; row++)
+                {
+                    txtBinaryImg = String.Format(txtBinaryImg + binaryImage[row, colunm]);
+                }
+            }
+        }
+
+        private void GetPixelsImage()
+        {
+            //armazenar cor
+            Color pixelColor;
+
+            for (int column = 0; column < heightImg; column++)
+            {
+                for (int row = 0; row < widthImg; row++)
+                {
+
+                    //pega o codigo da cor do pixel
+                    pixelColor = bitmap.GetPixel(row, column);
+
+                    if (pixelColor.ToArgb() == -16777216) //valor representa o preto
+                    {
+                        //cor preta registrada como 1
+                        binaryImage[row, column] = 1;
+                    }
+                    else
+                    {
+                        //cor branca registrada como 0
+                        binaryImage[row, column] = 0;
+                    }
+                }
+            }
+        }
+
+    }
+}
